80a9ca3b93432d146f8e11bb3576d250a27a7d6a
wangyunxizhe <543456229@qq.com>
2019-04-09 14:05:13 +0800

    顺序消息测试

---
+++ src/main/java/com/yuan/www/orderly/Consumer.java
@@ -0,0 +1,54 @@
+package com.yuan.www.orderly;
+
+import com.yuan.www.constants.Const;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerOrderly;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 顺序消息--消费者
+ */
+public class Consumer {
+
+    public Consumer() throws Exception {
+        String group_name = "test_orderly_consumer_name";
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer(group_name);
+        consumer.setNamesrvAddr(Const.NAMESRV_ADDR_MASTER_SLAVE);
+        //设置Consumer第一次启动是从队列头部开始消费还是队列尾部开始消费
+        //如果非第一次启动，那么按照上次消费的位置继续消费
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_LAST_OFFSET);
+        //订阅主题，标签
+        consumer.subscribe("test_order_topic", "TagA");
+        //注册监听
+        consumer.registerMessageListener(new Listener());
+        consumer.start();
+        System.out.println("Consumer Start...");
+    }
+
+    class Listener implements MessageListenerOrderly {
+
+        private Random random = new Random();
+
+        @Override
+        public ConsumeOrderlyStatus consumeMessage(List<MessageExt> msqs, ConsumeOrderlyContext context) {
+            //设置自动提交
+            context.setAutoCommit(true);
+            for (MessageExt msg : msqs) {
+                System.out.println(msg + ",context:" + new String(msg.getBody()));
+                try {
+                    //模拟业务逻辑处理中
+                    TimeUnit.SECONDS.sleep(random.nextInt(4) + 1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return ConsumeOrderlyStatus.SUCCESS;
+        }
+    }
+}
---
+++ src/main/java/com/yuan/www/orderly/Producer.java
@@ -0,0 +1,84 @@
+package com.yuan.www.orderly;
+
+import com.yuan.www.constants.Const;
+import org.apache.rocketmq.client.exception.MQBrokerException;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.MessageQueueSelector;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+import org.apache.rocketmq.remoting.exception.RemotingException;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 顺序消息--生产者
+ */
+public class Producer {
+
+    public static void main(String[] args) throws MQClientException, UnsupportedEncodingException,
+            RemotingException, InterruptedException, MQBrokerException {
+        String group_name = "test_orderly_producer_name";
+        DefaultMQProducer producer = new DefaultMQProducer(group_name);
+        producer.setNamesrvAddr(Const.NAMESRV_ADDR_MASTER_SLAVE);
+        producer.start();
+
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String dateStr = sdf.format(date);
+        //模拟一个大业务中需要发5条消息，而这5条消息必须是顺序的
+        for (int i = 0; i < 5; i++) {
+            String body = dateStr + " Orderly MQ " + i;
+            Message msg = new Message("test_order_topic", "TagA", "Key" + i,
+                    body.getBytes(RemotingHelper.DEFAULT_CHARSET));
+            //发送消息：如果要实现顺序消费，则必须实现自己的，保证消息进入到同一个队列中
+            SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                @Override
+                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                    Integer id = (Integer) arg;
+                    System.out.println("id：" + id);
+                    return mqs.get(id);
+                }
+            }, 1);//这里的参数1是队列的下标，也可以理解为将msg都发送到1号队列中
+            System.out.println(sendResult + ",body:" + body);
+        }
+        //第二波顺序消息
+        for (int i = 0; i < 5; i++) {
+            String body = dateStr + " Orderly MQ " + i;
+            Message msg = new Message("test_order_topic", "TagA", "Key" + i,
+                    body.getBytes(RemotingHelper.DEFAULT_CHARSET));
+            //发送消息：如果要实现顺序消费，则必须实现自己的，保证消息进入到同一个队列中
+            SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                @Override
+                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                    Integer id = (Integer) arg;
+                    System.out.println("id：" + id);
+                    return mqs.get(id);
+                }
+            }, 2);//发送到2号队列中
+            System.out.println(sendResult + ",body:" + body);
+        }
+        //第三波顺序消息
+        for (int i = 0; i < 5; i++) {
+            String body = dateStr + " Orderly MQ " + i;
+            Message msg = new Message("test_order_topic", "TagA", "Key" + i,
+                    body.getBytes(RemotingHelper.DEFAULT_CHARSET));
+            //发送消息：如果要实现顺序消费，则必须实现自己的，保证消息进入到同一个队列中
+            SendResult sendResult = producer.send(msg, new MessageQueueSelector() {
+                @Override
+                public MessageQueue select(List<MessageQueue> mqs, Message msg, Object arg) {
+                    Integer id = (Integer) arg;
+                    System.out.println("id：" + id);
+                    return mqs.get(id);
+                }
+            }, 3);//发送到3号队列中
+            System.out.println(sendResult + ",body:" + body);
+        }
+        producer.shutdown();
+    }
+
+}
